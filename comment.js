3cea3e67ed0923a4df1403a75f0c386fd33a5eef
Prasamsha Sharma <49678583+Prasamsha12@users.noreply.github.com>
2023-07-20 10:28:39 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,16 +1,1 @@
-// Create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
-// Connect to mongodb
-mongoose.connect('mongodb://localhost/comment');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function(){
-  console.log('Connected to mongodb');
-});
-
-// Define schema
-var commentSchema = mongoose.Schema({
+// Create web server and handle requests
